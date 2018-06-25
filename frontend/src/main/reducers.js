50839a732e67213feb29c7ce1f37659c45ee2b8e
Miguel Maia <miguel@cupdev.com>
2018-06-25 20:56:44 -0300

    Billing Cycle Reducer

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 
 const rootReducer = combineReducers({
-  dashboard: DashboardReducer,
-  tab : TabReducer
+  dashboard         : DashboardReducer,
+  tab               : TabReducer,
+  billingCycle      : BillingCycleReducer
 }); 
 
 export default rootReducer
