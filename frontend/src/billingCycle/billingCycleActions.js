65be5a9ce0a23929791b120154b6f7aa62dd4daf
Miguel Maia <miguel@cupdev.com>
2018-06-27 19:31:25 -0300

    criado componente form e estilizado

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { toastr } from 'react-redux-toastr'
 const BASE_URL = 'http://localhost:3003/api'
 
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabAction'
+
 export function getList(){
     const request = axios.get(`${BASE_URL}/billingCycles`)
     return {
@@ -11,14 +14,19 @@ export function getList(){
 }
 
 export function create(values){
-    axios.post(`${BASE_URL}/billingCycles`, values)
-        .then(resp => {
-            toastr.success('Sucesso', 'Operação realizada com sucesso')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        })
-    return {
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList','tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
     }
 }
