e9caf2c4d208876567b594f84f56be27ccde2978
Miguel Maia <miguel@cupdev.com>
2018-07-01 22:24:50 -0300

    Corrigido erro responsividade menu

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Router, Route, Redirect, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute ,Redirect, hashHistory } from 'react-router';
+
+import App from './app.jsx'
 import Dashboad from '../dashboard/dashboard';
 import BillingCycle from '../billingCycle/billingCycle';
 
 export default props => (
 
     <Router history={hashHistory}>
-        <Route path="/" component={Dashboad} />
-        <Route path="/billingcycles" component={BillingCycle} />
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboad}/>
+            <Route path="billingcycles" component={BillingCycle} />
+        </Route>
         <Redirect from="*" to="/" />
     </Router>
 
