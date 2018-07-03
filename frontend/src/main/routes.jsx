5cbc6822117f43e22d902715f8af7b11035ce689
Miguel Maia <miguel@cupdev.com>
2018-07-03 19:25:02 -0300

    Autenticação finalizada

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Router, Route, IndexRoute ,Redirect, hashHistory } from 'react-router';
 
-import App from './app.jsx'
+import AuthOrApp from './authOrApp'
 import Dashboad from '../dashboard/dashboard';
 import BillingCycle from '../billingCycle/billingCycle';
 
 export default props => (
-
-    <Router history={hashHistory}>
-        <Route path='/' component={App}>
-            <IndexRoute component={Dashboad}/>
-            <Route path="billingcycles" component={BillingCycle} />
-        </Route>
-        <Redirect from="*" to="/" />
-    </Router>
-
-)
+	<Router history={hashHistory}>
+		<Route path="/" component={AuthOrApp}>
+			<IndexRoute component={Dashboad} />
+			<Route path="billingcycles" component={BillingCycle} />
+		</Route>
+		<Redirect from="*" to="/" />
+	</Router>
+);
